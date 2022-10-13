25486f00faf78886f6b8c34f033e4684938deedb
Murillo Welsi <murillo.welsi@gmail.com>
2022-10-13 19:35:23 +0100

    improvements in logs

--- cypress/e2e/acessibility-test.cy.js
+++ cypress/e2e/acessibility-test.cy.js
@@ -1,7 +1,28 @@
-describe('Acessibility', () => {
-  it('should run acessibility audits', () => {
-    cy.visit('https://school.linuxtips.io/')
-    cy.injectAxe()
-    cy.checkA11y()
+/// <reference types="cypress" />
+
+
+
+
+
+describe("Acessibility", () => {
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
+    cy.visit("https://www.linuxtips.io/pt-pt");
+  });
+
+  it("should run acessibility audits on the landing page", () => {
+    cy.checkPageA11y()
+  });
+
+  it("should not have a11y violations in the Contato page", () => {
+    cy.contains("span", "Contato").click();
+    cy.checkPageA11y()
+  });
+
+  it("should not have a11y violations in the Contato page", () => {
+    cy.get(".card-wrapper").each(($el, index, $list) => {
+      cy.wrap($el).click();
+      cy.checkPageA11y()
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,35 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const severityIndicators = {
+  minor: "⚪️",
+  moderate: "🟡",
+  serious: "🟠",
+  critical: "🔴",
+};
+
+function callback(violations) {
+  violations.forEach((violation) => {
+    const nodes = Cypress.$(
+      violation.nodes.map((node) => node.target).join(",")
+    );
+
+    Cypress.log({
+      name: `${severityIndicators[violation.impact]} A11Y`,
+      consoleProps: () => violation,
+      $el: nodes,
+      message: `[${violation.help}](${violation.helpUrl})`,
+    });
+
+    violation.nodes.forEach(({ target }) => {
+      Cypress.log({
+        name: "🔧",
+        consoleProps: () => violation,
+        $el: Cypress.$(target.join(",")),
+        message: target,
+      });
+    });
+  });
+}
+
+Cypress.Commands.add("checkPageA11y", () => {
+  cy.injectAxe();
+  cy.checkA11y(null, null, callback);
+});
